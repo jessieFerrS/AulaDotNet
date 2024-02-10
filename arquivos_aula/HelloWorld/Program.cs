f1773493465529bbb07348b6a133047945e498b8
jessieFerrS <114115950+jessieFerrS@users.noreply.github.com>
2024-02-10 13:46:35 -0300

    Add #1 Class .Net
    Introdução a linguagem .Net e C#, para aprendizado de desenvolvimento de Sistemas de Informação.

---
+++ arquivos_aula/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld; //nome do projeto
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        int n1 = 1;
+        int n2 = 1;
+        Console.WriteLine(n1);
+        Console.WriteLine(n2);
+        
+        for (int i=2; i <= 10; i++){
+            int res = n1 + n2;
+            Console.WriteLine(res);
+            n1 = n2;
+            n2 = res;
+        }
+    }
+}

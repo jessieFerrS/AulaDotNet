048f22e3e60ad07cf9b201975ffb25ac81fa9a28
jessieFerrS <114115950+jessieFerrS@users.noreply.github.com>
2024-03-12 22:05:50 -0300

    Primeiro Update AulaDotNet

---
+++ arquivos_aula/classificandoNumeros/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace classificandoNumeros;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int>pares = new List<int>();
+        List<int>impares = new List<int>();
+        for (int i = 0; i <= 100; i++)
+        {
+            if(i % 2 == 0)
+            {
+                pares.Add(i);
+                //Console.WriteLine($"{i} - Par");
+            }
+            else{
+                impares.Add(i);
+                //Console.WriteLine($"{i} - Impar");
+            }
+        }
+        Console.WriteLine("Valores Pares: " + string.Join(" , ", pares));
+        Console.WriteLine("Valores Impares: " + string.Join(" , ", impares));
+    }
+}
